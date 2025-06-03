fcb9a7e2b9b82dbac1fc107431f0db37ac92dc30
Drax54 <dhcp73@gmail.com>
2025-06-03 18:23:28 +0100

    new

--- components/QuizPageClient.tsx
+++ components/QuizPageClient.tsx
@@ -78,6 +78,12 @@ export default function QuizPageClient({ quiz, categoryId }: QuizPageClientProps
 
     setIsQuizCompleted(true)
     setShowResult(true)
+    
+    // Scroll to top when results are shown
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
   }
 
   const scrollToContent = () => {
